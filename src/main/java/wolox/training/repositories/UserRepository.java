1f708f10093947368f3b5b8dd04742ca1ce833ee
Ignacio Coluccio <ignacio.coluccio@gmail.com>
2020-03-13 18:28:11 -0300

    Added user repository

---
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package wolox.training.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import wolox.training.models.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+  User findByUsername(String username);
+}
